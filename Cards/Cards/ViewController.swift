7b756205571a59c9f4ec7cc2ffe5e21ac96f989a
ellscha <escharlin@wesleyan.edu>
2016-07-14 10:30:38 -0400

    done, not so pretty, but it works.

--- Cards/Cards/ViewController.swift
+++ Cards/Cards/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var suitTopLeft: UILabel!
+    @IBOutlet weak var suitBottomRight: UILabel!
+    @IBOutlet weak var numCenter: UILabel!
     // TODO: Create IB outlets
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    @IBAction func fourTapped(sender: AnyObject) {
+        numCenter.text = "4"
+        suitTopLeft.text = "♠️"
+        suitBottomRight.text = "♠️"
+    }
+    @IBAction func queenTapped(sender: AnyObject) {
+        numCenter.text = "Q"
+        suitTopLeft.text = "♥️"
+        suitBottomRight.text = "♥️"
+    }
+    @IBAction func nineTapped(sender: AnyObject) {
+        numCenter.text = "9"
+        suitTopLeft.text = "♣️"
+        suitBottomRight.text = "♣️"
+    }
+    @IBAction func tenTapped(sender: AnyObject) {
+        numCenter.text = "10"
+        suitTopLeft.text = "♦️"
+        suitBottomRight.text = "♦️"
     }
 
     // TODO: IB actions and code to update UI
